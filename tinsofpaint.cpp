7b46fde7c04d50181e77fdedc8733be05df0ccbf
Adrian Smith <147835901+adismith2023@users.noreply.github.com>
2023-12-10 19:24:26 +0000

    Add files via upload

---
+++ tinsofpaint.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <cmath> // For ceil function
+//using namespace std;
+//
+//int main()
+//{
+//	// Coverage per tin
+//	int coveragePerTin = 340;
+//	// Fence dimentions
+//	int fenceHeight = 6;
+//	int fenceWidth = 100;
+//
+//	// Calculate the square footage of the fence
+//	int squareFeet = fenceHeight * fenceWidth;
+//
+//	// Calculate how many tins of paint we need (rounded up)
+//	int tinsOfPaint = ceil(static_cast<double>(squareFeet) / coveragePerTin);
+//
+//	// Print the result
+//	cout << "for your fence which is " << squareFeet << "sqft you will need " << tinsOfPaint << " tins of paint" << endl;
+//
+//	return 0;
+//}
